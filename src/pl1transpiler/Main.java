813d96077a2c2f3e4857a1990bb6f8c2b9e59bb1
lhahner <71670918+lhahner@users.noreply.github.com>
2024-02-07 11:11:27 +0100

    Add files via upload

---
+++ src/pl1transpiler/Main.java
@@ -0,0 +1,84 @@
+package pl1transpiler;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import pl1transpiler.generated.Pl1Parser;
+import pl1transpiler.generated.SimpleNode;
+import pl1transpiler.tools.Pl1.SymbolTable;
+
+public class Main {
+	static String inputFile;
+	static Lexer lex;
+	static Pl1Parser pl1Parser;
+
+	/**
+	 * @param args[0] holds the path to the input file.
+	 */
+	public static void main(String[] args) {
+		inputFile = args[0];
+		String prefixUrl = args[0].substring(0, 56);
+		SymbolTable st = new SymbolTable();
+		String content = "";
+		String token = "";
+
+		try {
+			File pl1File = new File(inputFile);
+			File throughputFile = new File(prefixUrl + "/src/pl1transpiler/var/code_tp.pli");
+			FileInputStream inputStream = new FileInputStream(pl1File);
+			FileWriter writer = new FileWriter(throughputFile);
+			Scanner sc = new Scanner(inputStream);
+			lex = new Lexer();
+
+			while (sc.hasNext()) {
+				content = sc.useDelimiter(("[.:?!\"]+")).next();
+				content = content.replaceAll("/*[A-Z, a-z, 0-9, *, \n, \t, \r]*/", "");
+				content = content.replaceAll(";", " \n");
+				content = content.replaceAll("\\),", "), ");
+				content = content.replaceAll(",\n", ", ");
+			}
+			while (content.length() > 0) {
+
+				if (lex.getToken(content) == "failed") {
+					break;
+				}
+
+				token = lex.getToken(content);
+				writer.write(token + " ");
+				content = content.substring(token.length()).trim();
+			}
+			sc.close();
+			writer.close();
+			FileInputStream parseInputStream = new FileInputStream(throughputFile);
+
+			pl1Parser = new Pl1Parser(parseInputStream);
+			SimpleNode root = pl1Parser.program();
+			st.printAll();
+			root.dump(" ");
+
+			// load Java Parser and give Pl1parser
+			JavaGenerator jP = new JavaGenerator(pl1Parser);
+
+			// create expression with Parsetree
+			jP.createExpression(root);
+
+			System.out.println(jP.concatExpression());
+			File javaFile = new File(prefixUrl + "/src/pl1transpiler/output/Main.java");
+			FileWriter writeFile = new FileWriter(javaFile);
+			writeFile.write(jP.concatExpression());
+			writeFile.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ src/pl1transpiler/output/Main.java
@@ -0,0 +1,59 @@
+package pl1transpiler.output; 
+import java.math.BigDecimal; import java.util.HashMap; 
+ import java.util.HashMap; 
+ import java.util.regex.Matcher; 
+ import java.util.regex.Pattern;import pl1transpiler.tools.Pl1.PictureMapper; 
+public class Main { 
+public static void main(String[] args) { }; private  PL1_CHAR test = new  PL1_CHAR(10); 
+} 
+interface Datatype { 
+public void init(String content); 
+}; 
+ 
+class PICTURE {
+	Pattern pattern;
+	String content;
+
+	public PICTURE(String regex) {
+		PictureMapper picmap = new PictureMapper();
+		String regExpression = "";
+		if(regex.contains("(")) {
+			regExpression = picmap.translateLengthExpression(regex);
+		}
+		else {
+			for(int i = 0; i<regex.length();i++) {	
+				regExpression = regExpression + picmap.getRegex(regex.charAt(i));
+			}
+		}
+		pattern = Pattern.compile(regExpression);
+	}
+
+	public void init(String content) {
+		Matcher matcher = pattern.matcher(content);
+		if (matcher.matches()) {
+			this.content = content;
+		}
+	}
+} 
+class PL1_CHAR implements Datatype{ char[] varname; public PL1_CHAR(int limit) { 
+varname =  new char[limit]; } 
+@Override public String toString() { 
+ String string = new String(varname); 
+return string; } 
+public void init(String varname) { 
+for(int i = 0; i<this.varname.length; i++) { 
+this.varname[i] = varname.charAt(i); }}} 
+ 
+class BINARY implements Datatype {
+	byte[] content;
+	
+	public BINARY(int limit) {
+		content = new byte[limit];
+	}
+	
+	public void init(String content) {
+		for(int i = 0; i<this.content.length;i++) {
+			this.content[i] = (byte)content.charAt(i);
+		}
+	}
+} 
