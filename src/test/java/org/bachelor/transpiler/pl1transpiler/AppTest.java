3721914a5af098d59d0f3778515b8436dca71205
Hahner, Lennart <lennart.hahner@accenture.com>
2024-05-02 11:54:55 +0200

    migrated project to maven

---
+++ src/test/java/org/bachelor/transpiler/pl1transpiler/AppTest.java
@@ -0,0 +1,20 @@
+package org.bachelor.transpiler.pl1transpiler;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
